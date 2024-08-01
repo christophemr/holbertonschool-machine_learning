5dbc0b6772612d2d282ebc3cbf2b335f0bd14cc5
christophemr <christophemr59@gmail.com>
2024-08-01 12:12:27 +0200

    tasks 10, 11 & 12

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""function that transposes a matrix"""
+
+
+def np_transpose(matrix):
+    """returns and transposes a numpy.ndarray"""
+    return matrix.transpose()
+
---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""function that performs elementary operations"""
+
+
+def np_elementwise(mat1, mat2):
+    """performs element-wise addition, subtraction, multiplication, and division on 2matrices"""
+    add = mat1 + mat2
+    sub = mat1 - mat2
+    mul = mat1 * mat2
+    div = mat1 / mat2
+    return (add, sub, mul, div)
