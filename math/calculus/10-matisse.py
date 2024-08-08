ba0163b1a32344faac51193837da56001e5eaf7e
christophemr <christophemr59@gmail.com>
2024-08-08 21:48:53 +0200

    function that calculates the derivative of a polynomial

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""function that calculates the derivative
+of a polynomial
+"""
+
+
+def poly_derivative(poly):
+    """
+    Calculates the derivative of a polynomial
+
+    Args:
+    poly (list): List of coefficients representing a polynomial
+
+    Returns:
+    list: A new list of coefficients representing
+    the derivative of the polynomial
+    None: If poly is not valid
+    """
+    if (not isinstance(poly, list) or
+       not all(isinstance(coef, (int, float)) for coef in poly)):
+        return None
+
+    if len(poly) <= 1:
+        return [0]
+
+    derivative = [i * poly[i] for i in range(1, len(poly))]
+    return derivative
