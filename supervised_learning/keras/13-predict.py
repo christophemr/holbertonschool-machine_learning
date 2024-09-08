e8234a98d9564a54ed892e412bc16cb42b869b19
christophemr <christophemr59@gmail.com>
2024-09-08 21:28:49 +0200

    task 13

---
+++ supervised_learning/keras/13-predict.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Defines a function that makes a prediction using neural network
+using Keras library
+"""
+
+import tensorflow.keras as K
+
+
+def predict(network, data, verbose=False):
+    """
+    Makes a prediction using a neural network
+
+    parameters:
+        network [keras model]: model to make prediction with
+        data: input data to make prediction with
+        verbose [boolean]:
+            determines if output should be printed during prediction process
+
+    returns:
+        the prediction for the data
+    """
+    return network.predict(data, verbose=verbose)
