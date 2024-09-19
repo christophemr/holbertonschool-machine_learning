c1389332568280e9470845b7ea3516438c900642
christophemr <christophemr59@gmail.com>
2024-09-19 10:23:43 +0200

    that creates a neural network layer in tensorFlow that includes L2 regularization

---
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+Defines a function that creates a TensorFlow layer
+that includes L2 Regularization
+"""
+
+import tensorflow as tf
+
+
+def l2_reg_create_layer(prev, n, activation, lambtha):
+    """
+    Creates a TensorFlow layer that includes L2 regularization
+
+    Parameters:
+    prev (tensor): Output of the previous layer.
+    n (int): Number of nodes in the new layer.
+    activation (function): Activation function for the new layer.
+    lambtha (float): L2 regularization parameter.
+
+    Returns:
+    tensor: Output of the new layer.
+    """
+    # Define L2 regularizer
+    l2_reg = tf.keras.regularizers.L2(lambtha)
+
+    # Use VarianceScaling for kernel initialization
+    initializer = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+
+    # Create the dense layer
+    layer = tf.keras.layers.Dense(
+        units=n,
+        activation=activation,
+        kernel_initializer=initializer,
+        kernel_regularizer=l2_reg
+    )
+
+    # Return the output of the layer
+    return layer(prev)
