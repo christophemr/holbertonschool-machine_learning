43c59e8dbeb0437ab391375ba20b674264ce301d
christophemr <christophemr59@gmail.com>
2024-11-07 17:14:28 +0100

    method def pdf that calculates the value of the PDF for a gieven value added to the class

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -70,3 +70,20 @@ class Normal:
             float: x-value corresponding to z
         """
         return z * self.stddev + self.mean
+
+    def pdf(self, x):
+        """
+        Calculates the value of the PDF for a given x-value
+
+        Parameters:
+            x (float): x-value
+
+        Returns:
+            float: PDF value for x
+        """
+        e = 2.7182818285
+        pi = 3.1415926536
+        coefficient = 1 / (self.stddev * (2 * pi) ** 0.5)
+        exponent = -((x - self.mean) ** 2) / (2 * self.stddev ** 2)
+        pdf_value = coefficient * (e ** exponent)
+        return pdf_value
