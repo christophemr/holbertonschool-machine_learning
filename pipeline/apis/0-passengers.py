f6483d95517ebf2dbb81a4fdae44d0bdea5c6bfd
christophemr <christophemr59@gmail.com>
2025-03-20 10:28:02 +0100

    methods to ping the Star Wars API and return the list of ships that can hold a given number of passengers

---
+++ pipeline/apis/0-passengers.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Defines methods to ping the Star Wars API and return the list of ships
+that can hold a given number of passengers
+"""
+
+import requests
+
+
+def availableShips(passengerCount):
+    """
+    Returns a list of ships that can hold a given number of passengers.
+
+    Args:
+        passengerCount (int): The number of passengers to accommodate.
+
+    Returns:
+        list: List of ship names that can hold the given number of passengers.
+    """
+    ships = []
+    next_page = "https://swapi.py4e.com/api/starships/"
+
+    while next_page:
+        response = requests.get(next_page)
+        data = response.json()
+
+        # Iterate over the results and filter ships by passenger capacity
+        for ship in data['results']:
+            # Check if the passengers field is a valid number
+            if ship['passengers'].isdigit():
+                if int(ship['passengers']) >= passengerCount:
+                    ships.append(ship['name'])
+
+        # Check for the next page URL
+        next_page = data['next']
+
+    return ships
