7a505db5b7c9c9f9056b7b3b127c60e5059897dc
christophemr <christophemr59@gmail.com>
2025-03-25 10:55:11 +0100

    Fix typo (rating → rate) and use LEFT JOIN for missing ratings

--- pipeline/databases/11-rating_shows.sql
+++ pipeline/databases/11-rating_shows.sql
@@ -2,8 +2,8 @@
 -- uses hbtn_0d_tvshows_rate database
 -- database name will be passed as an argument of the mysql command
 
-SELECT tv_shows.title, SUM(tv_show_ratings.rating) AS rating_sum
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
 FROM tv_shows
-JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
+LEFT JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_shows.title
-ORDER BY rating_sum DESC;
+ORDER BY rating DESC;
